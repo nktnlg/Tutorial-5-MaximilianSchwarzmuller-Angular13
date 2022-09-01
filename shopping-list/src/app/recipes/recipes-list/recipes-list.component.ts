26d38b1bba1415b45f774759b8563dd223591241
nktnlg <2eleven94@gmail.com>
2022-09-02 02:38:13 +0300

    lesson 60

--- shopping-list/src/app/recipes/recipes-list/recipes-list.component.ts
+++ shopping-list/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test recipe', 'https://loveincorporated.blob.core.windows.net/contentimages/gallery/d9e900e4-212e-4c3d-96d5-cb14a023c659-worlds-most-delicious-dishes.jpg'),
+    new Recipe('A Test Recipe', 'This is a test recipe', 'https://loveincorporated.blob.core.windows.net/contentimages/gallery/d9e900e4-212e-4c3d-96d5-cb14a023c659-worlds-most-delicious-dishes.jpg')
+    
+  ];
 
   constructor() { }
 
