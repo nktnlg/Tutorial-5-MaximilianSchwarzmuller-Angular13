04db9b0e7337df61ba77538b8918cdcb25438838
nktnlg <2eleven94@gmail.com>
2022-09-01 13:10:04 +0300

    lesson 43

---
+++ shopping-list/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styles: [
+  ]
+})
+export class RecipesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
